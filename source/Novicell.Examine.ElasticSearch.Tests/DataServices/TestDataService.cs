a403d6bbbbe105f6eea784694aa1fd9a8ca0d82a
arkadiuszbiel <abi@novicell.co.uk>
2019-09-27 10:49:35 +0100

    slowly adapt examine unit tests to works with elastic provider - use inside elastic

---
+++ source/Novicell.Examine.ElasticSearch.Tests/DataServices/TestDataService.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Examine.Test;
+using Examine.Test.DataServices;
+
+namespace Novicell.Examine.ElasticSearch.Tests.DataServices
+{
+    public class TestDataService : IDataService
+    {
+
+        public TestDataService()
+        {
+            ContentService = new TestContentService();
+            LogService = new TestLogService();
+            MediaService = new TestMediaService();
+        }
+
+        #region IDataService Members
+
+        public IContentService ContentService { get; private set; }
+
+        public ILogService LogService { get; private set; }
+
+        public IMediaService MediaService { get; private set; }
+
+        public string MapPath(string virtualPath)
+        {
+            return new DirectoryInfo(TestHelper.AssemblyDirectory) + "\\" + virtualPath.Replace("/", "\\");
+        }
+
+        #endregion
+    }
+}
---
+++ source/Novicell.Examine.ElasticSearch.Tests/TestIndex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Examine.LuceneEngine;
+using Examine.LuceneEngine.Indexing;
+using Examine.LuceneEngine.Providers;
+using Lucene.Net.Analysis;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+using Novicell.Examine.ElasticSearch;
+
+namespace Examine.Test
+{
+    public class TestIndex : ElasticSearchIndex
+    {
+        public TestIndex(FieldDefinitionCollection fieldDefinitions, Directory luceneDirectory, Analyzer analyzer, IValueSetValidator validator = null, IReadOnlyDictionary<string, IFieldValueTypeFactory> indexValueTypesFactory = null)
+            : base("testIndexer", luceneDirectory, fieldDefinitions, analyzer, validator, indexValueTypesFactory)
+        {
+            RunAsync = false;
+        }
+
+        public TestIndex(Directory luceneDirectory, Analyzer defaultAnalyzer, IValueSetValidator validator = null)
+            : base("testIndexer", luceneDirectory, new FieldDefinitionCollection(), defaultAnalyzer, validator)
+        {
+            RunAsync = false;
+        }
+
+        public TestIndex(IndexWriter writer, IValueSetValidator validator = null)
+            : base("testIndexer", new FieldDefinitionCollection(), writer, validator)
+        {
+        }
+
+        public IEnumerable<ValueSet> AllData()
+        {
+            var data = new List<ValueSet>();
+            for (int i = 0; i < 100; i++)
+            {
+                data.Add(ValueSet.FromObject(i.ToString(), "category" + (i % 2), new { item1 = "value" + i, item2 = "value" + i }));
+            }
+            return data;
+        }
+    }
+}

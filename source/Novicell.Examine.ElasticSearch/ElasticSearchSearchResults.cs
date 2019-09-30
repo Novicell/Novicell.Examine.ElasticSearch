a4f91aa4b6c045320914f20d30b53c68cddcee93
Bhavesh Valand <Valand.Bhavesh@bcg.com>
2019-09-30 17:27:10 +0100

    Added sorting using lucene.sortfields

--- source/Novicell.Examine.ElasticSearch/ElasticSearchSearchResults.cs
+++ source/Novicell.Examine.ElasticSearch/ElasticSearchSearchResults.cs
@@ -9,6 +9,7 @@ using Lucene.Net.Search;
 using Nest;
 using Novicell.Examine.ElasticSearch.Helpers;
 using Novicell.Examine.ElasticSearch.Model;
+using SortField = Lucene.Net.Search.SortField;
 
 namespace Novicell.Examine.ElasticSearch
 {
@@ -18,28 +19,32 @@ namespace Novicell.Examine.ElasticSearch
         private readonly ElasticClient _client;
         private readonly BooleanQuery _luceneQuery;
         private QueryContainer _queryContainer;
+        private SortDescriptor<Document> _sortDescriptor;
         private readonly string _indexName;
         private IEnumerable<ISearchResult> results;
         public long TotalItemCount { get; private set; }
 
 
-        public ElasticSearchSearchResults(ElasticClient client, BooleanQuery luceneQuery, string indexName,
+        public ElasticSearchSearchResults(ElasticClient client, BooleanQuery luceneQuery, string indexName, List<SortField> sortFields,
             int? maxResults = null)
         {
             _luceneQuery = luceneQuery ?? throw new ArgumentNullException(nameof(luceneQuery));
             _maxResults = maxResults;
             _client = client;
             _indexName = indexName;
+            _sortDescriptor = GetSortDescriptor(sortFields);
+
             results = DoSearch(null).ConvertResult();
         }
 
-        public ElasticSearchSearchResults(ElasticClient client, QueryContainer queryContainer, string indexName,
-            int? maxResults = null)
+        public ElasticSearchSearchResults(ElasticClient client, QueryContainer queryContainer, string indexName, List<SortField> sortFields, int? maxResults = null)
         {
             _queryContainer = queryContainer ?? throw new ArgumentNullException(nameof(queryContainer));
             _maxResults = maxResults;
             _client = client;
             _indexName = indexName;
+            _sortDescriptor = GetSortDescriptor(sortFields);
+
             results = DoSearch(null).ConvertResult();
         }
 
@@ -67,18 +72,18 @@ namespace Novicell.Examine.ElasticSearch
                 });
             }
 
-
+           
             SearchDescriptor<Document> searchDescriptor = new SearchDescriptor<Document>();
+           
             searchDescriptor.Index(_indexName)
                 .Skip(skip)
                 .Size(_maxResults)
-                .Query(q => _queryContainer);
+                .Query(q => _queryContainer)
+                .Sort(s => _sortDescriptor);
 
             var json = _client.RequestResponseSerializer.SerializeToString(searchDescriptor);
             searchResult = _client.Search<Document>(searchDescriptor);
 
-
-            //TODO: Get sorting working
             //TODO: Get filtering/range working
             //TODO: We need to escape the resulting query
 
@@ -91,6 +96,18 @@ namespace Novicell.Examine.ElasticSearch
         {
             results = DoSearch(skip).ConvertResult();
             return results;
+        }
+
+        public SortDescriptor<Document> GetSortDescriptor(List<SortField> fields)
+        {
+            SortDescriptor<Document> sortDescriptor = new SortDescriptor<Document>();
+
+            foreach (var field in fields)
+            {
+                sortDescriptor.Field(field.Field, field.Reverse ? SortOrder.Descending : SortOrder.Ascending);
+            }
+
+            return sortDescriptor;
         }
     }
 }

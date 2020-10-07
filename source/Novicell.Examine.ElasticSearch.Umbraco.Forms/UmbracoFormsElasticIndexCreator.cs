b932bd47f417d259cab5af3bbc5e685235bbf3a3
Arkadiusz Biel <abi@novicell.co.uk>
2020-10-07 19:47:53 +0100

    add forms indexer

---
+++ source/Novicell.Examine.ElasticSearch.Umbraco.Forms/UmbracoFormsElasticIndexCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Examine;
+using Umbraco.Forms.Core.Data.RecordIndex;
+
+namespace Novicell.Examine.ElasticSearch.Umbraco.Forms
+{
+    public class UmbracoFormsElasticIndexCreator : IFormsIndexCreator
+    {
+        public IEnumerable<IIndex> Create() => (IEnumerable<IIndex>) new IIndex[1]
+        {
+            this.CreateElasticRecordIndex()
+        };
+
+        private IIndex CreateElasticRecordIndex() => (IIndex) new UmbracoFormsElasticIndex("UmbracoFormsRecordsIndex",
+            ElasticSearchConfig.GetConfig("FormsConfig"),
+            new FieldDefinitionCollection(Enumerable.Empty<FieldDefinition>()), "simple");
+
+    }
+}
